27e0661d108761dd46c7a2cc9de0195c5bc3fe20
Vipin-chauhan <vipin.chauhan@thewitslab.com>
2023-11-23 00:25:08 +0530

    implement stats filter, searching,  sorting filter and pagination filter

---
+++ client/src/pages/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const NotFound = () => {
+  return (
+    <>
+    <h1>Page not found</h1>
+    <Link className='btn btn-success' to='/'>Go Back</Link>
+    </>
+  )
+}
+
+export default NotFound
