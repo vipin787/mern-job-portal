0aff48b94f8e133307d361cde68ee4fa19022ccc
Vipin-chauhan <vipin.chauhan@thewitslab.com>
2023-11-23 00:33:21 +0530

    security fix nodejs and implement api ocumentation using swagger

--- routes/jobsRoutes.js
+++ routes/jobsRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createJobController, deleteJobController, getAllJobsController, updateJobController } from '../controllers/jobsController.js';
+import { createJobController, deleteJobController, getAllJobsController, jobStatsController, updateJobController } from '../controllers/jobsController.js';
 import userAuth from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
@@ -20,6 +20,6 @@ router.delete('/delete-job/:id', userAuth, deleteJobController)
 
 // JOBS STATS FILTER || GET
 
-router.get('/job-stats', userAuth, deleteJobController)
+router.get('/job-stats', userAuth, jobStatsController)
 
 export default router;
--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+import swaggerUi from 'swagger-ui-express'
+import swaggerDoc from 'swagger-jsdoc';
 // import
 import express from "express";
 import "express-async-errors";
@@ -5,6 +7,12 @@ import dotenv from "dotenv";
 import colors from "colors";
 import cors from "cors";
 import morgan from "morgan";
+
+// security packages
+import helmet from 'helmet'
+
+import xss from 'xss-clean'
+import mongoSanitize from 'express-mongo-sanitize'
 
 // files imports
 import connectDB from "./config/db.js";
@@ -24,6 +32,24 @@ dotenv.config();
 // mongodb connection
 connectDB();
 
+// swagger api config 
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Job Portal Application",
+      description: "Node Expressjs Job Portal Application"
+    },
+    servers: [
+      {
+        url: "http://localhost:8080",
+      },
+    ],
+  },
+  apis: ["./routes/*.js"]
+}
+
+const spec = swaggerDoc(options)
 // rest objects
 const app = express();
 
@@ -31,6 +57,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(morgan("dev"));
+app.use(helmet())
+app.use(xss())
+app.use(mongoSanitize())
+
 
 // port
 const PORT = process.env.PORT || 8080;
@@ -40,6 +70,9 @@ app.use("/api/v1/test", testRoutes);
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/user", userRoutes);
 app.use("/api/v1/job", jobsRoutes);
+
+// homeRoute root
+app.use("/api-doc", swaggerUi.serve, swaggerUi.setup(spec));
 
 // validation middleware
 app.use(errorMiddleware);
