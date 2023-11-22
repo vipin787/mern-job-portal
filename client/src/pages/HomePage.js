320883d874e472f3fbef6a696e16f973d060c1e1
Vipin-chauhan <vipin.chauhan@thewitslab.com>
2023-11-23 00:38:43 +0530

    create login and register form an implement routing

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const HomePage = () => {
-  return (
-    <div className='text-primary'>HomePage</div>
-  )
-}
+  return <div className="text-primary">HomePage</div>;
+};
 
-export default HomePage
+export default HomePage;
--- client/src/pages/NotFound.js
+++ client/src/pages/NotFound.js
@@ -1,13 +1,15 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return (
     <>
-    <h1>Page not found</h1>
-    <Link className='btn btn-success' to='/'>Go Back</Link>
+      <h1>Page not found</h1>
+      <Link className="btn btn-success" to="/">
+        Go Back
+      </Link>
     </>
-  )
-}
+  );
+};
 
-export default NotFound
+export default NotFound;
